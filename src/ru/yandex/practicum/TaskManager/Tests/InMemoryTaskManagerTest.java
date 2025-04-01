2a6d3d2db8d9486e23c9466d1e3ad743adfebc24
Kirill Desyatnichenko <desyatnichenkokirill@yandex.ru>
2025-04-01 17:25:50 +0400

    Версия менеджера задач Фин.задание 6 спринта. Исправлены опечатки в наименовании переменных в тестах.

--- src/ru/yandex/practicum/TaskManager/Tests/InMemoryTaskManagerTest.java
+++ src/ru/yandex/practicum/TaskManager/Tests/InMemoryTaskManagerTest.java
@@ -17,7 +17,7 @@ class InMemoryTaskManagerTest {
     InMemoryTaskManager manager;
 
     @BeforeEach
-    void StarterPackage() {
+    void starterPackage() {
         manager = Managers.getDefault();
     }
 
@@ -42,11 +42,11 @@ class InMemoryTaskManagerTest {
     @Test
     void testSubTaskEqualityBasedOnId() {
         Epic epic1 = manager.createNewEpic(new Epic("Epic 1", "E", 1, TaskStatus.NEW));
-        SubTask SubTask1 = manager.createNewSubTask(new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1));
-        SubTask SubTask2 = manager.getSubTaskById(2);
-        SubTask SubTask3 = new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1);
-        assertEquals(SubTask1,SubTask2, "Одинаковые по ID подзадачи не равны");
-        assertEquals(SubTask2,SubTask3, "Одинаковые по ID подзадачи не равны");
+        SubTask subTask1 = manager.createNewSubTask(new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1));
+        SubTask subTask2 = manager.getSubTaskById(2);
+        SubTask subTask3 = new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1);
+        assertEquals(subTask1,subTask2, "Одинаковые по ID подзадачи не равны");
+        assertEquals(subTask2,subTask3, "Одинаковые по ID подзадачи не равны");
     }
 
     @Test
@@ -72,7 +72,7 @@ class InMemoryTaskManagerTest {
     @Test
     void testSubtaskFieldsAreUnchangedAfterAddingToManager() {
         Epic epic1 = manager.createNewEpic(new Epic("Epic 1", "E", 1, TaskStatus.NEW));
-        SubTask SubTask1 = manager.createNewSubTask(new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1));
+        SubTask subTask1 = manager.createNewSubTask(new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1));
         SubTask testSubTask = manager.getSubTaskById(2);
         assertEquals("Subtask 1", testSubTask.getTaskName(),"Имя задачи ошибочно");
         assertEquals("S1", testSubTask.getDescription(),"Описание задачи ошибочно");
@@ -86,11 +86,11 @@ class InMemoryTaskManagerTest {
         Task task1 = manager.createNewTask(new Task("Task 1", "T", 1, TaskStatus.NEW));
         Task task2 = manager.createNewTask(new Task("Task 2", "T2", 2, TaskStatus.IN_PROGRESS));
 
-        ArrayList<Task> TestTasks = manager.getAllTasks();
+        ArrayList<Task> testTasks = manager.getAllTasks();
 
-        assertEquals(2, TestTasks.size(), "Размер списка задач ошибочен");
-        assertTrue(TestTasks.contains(task1), "В списке отсутствует задача 1");
-        assertTrue(TestTasks.contains(task2), "В списке отсутствует задача 2");
+        assertEquals(2, testTasks.size(), "Размер списка задач ошибочен");
+        assertTrue(testTasks.contains(task1), "В списке отсутствует задача 1");
+        assertTrue(testTasks.contains(task2), "В списке отсутствует задача 2");
 
         Task updatedTask = new Task("NewTask 1", "Новое описание", 1, TaskStatus.DONE);
         manager.updateTask(updatedTask);
@@ -106,11 +106,11 @@ class InMemoryTaskManagerTest {
     @Test
     void testGetMapAddAndDeleteEpicAndSubtaskTheirInteraction() {
         Epic epic1 = manager.createNewEpic(new Epic("Epic 1", "E", 1, TaskStatus.NEW));
-        SubTask SubTask1 = manager.createNewSubTask(new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1));
-        SubTask SubTask2 = manager.createNewSubTask(new SubTask("Subtask 2", "S2", 3, TaskStatus.NEW, 1));
+        SubTask subTask1 = manager.createNewSubTask(new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1));
+        SubTask subTask2 = manager.createNewSubTask(new SubTask("Subtask 2", "S2", 3, TaskStatus.NEW, 1));
         Epic epic2 = manager.createNewEpic(new Epic("Epic 1", "E", 4, TaskStatus.NEW));
-        SubTask SubTask3 = manager.createNewSubTask(new SubTask("Subtask 3", "S3", 5, TaskStatus.NEW, 4));
-        SubTask SubTask4 = manager.createNewSubTask(new SubTask("Subtask 4", "S2", 6, TaskStatus.NEW, 4));
+        SubTask subTask3 = manager.createNewSubTask(new SubTask("Subtask 3", "S3", 5, TaskStatus.NEW, 4));
+        SubTask subTask4 = manager.createNewSubTask(new SubTask("Subtask 4", "S2", 6, TaskStatus.NEW, 4));
 
         ArrayList<Epic> testEpics = manager.getAllEpic();
         ArrayList<SubTask> testSubTasks = manager.getAllSubTasks();
@@ -120,37 +120,37 @@ class InMemoryTaskManagerTest {
         assertTrue(testEpics.contains(epic2), "В списке отсутствует эпик 2");
 
         assertEquals(4, testSubTasks.size(), "Размер списка подзадач ошибочен");
-        assertTrue(testSubTasks.contains(SubTask1), "В списке отсутствует подзадача 1");
-        assertTrue(testSubTasks.contains(SubTask2), "В списке отсутствует подзадача 2");
-        assertTrue(testSubTasks.contains(SubTask3), "В списке отсутствует подзадача 3");
-        assertTrue(testSubTasks.contains(SubTask4), "В списке отсутствует подзадача 4");
+        assertTrue(testSubTasks.contains(subTask1), "В списке отсутствует подзадача 1");
+        assertTrue(testSubTasks.contains(subTask2), "В списке отсутствует подзадача 2");
+        assertTrue(testSubTasks.contains(subTask3), "В списке отсутствует подзадача 3");
+        assertTrue(testSubTasks.contains(subTask4), "В списке отсутствует подзадача 4");
 
         ArrayList<SubTask> epic1SubTasks = epic1.getSubTasks();
-        assertTrue(epic1SubTasks.contains(SubTask1), "В эпике 1 отсутствует подзадача 1");
-        assertTrue(epic1SubTasks.contains(SubTask2), "В эпике 1 отсутствует  подзадача 2");
+        assertTrue(epic1SubTasks.contains(subTask1), "В эпике 1 отсутствует подзадача 1");
+        assertTrue(epic1SubTasks.contains(subTask2), "В эпике 1 отсутствует  подзадача 2");
 
         ArrayList<SubTask> epic2SubTasks = epic2.getSubTasks();
-        assertTrue(epic2SubTasks.contains(SubTask3), "В эпике 2 отсутствует  подзадача 1");
-        assertTrue(epic2SubTasks.contains(SubTask4), "В эпике 2 отсутствует подзадача 2");
+        assertTrue(epic2SubTasks.contains(subTask3), "В эпике 2 отсутствует  подзадача 1");
+        assertTrue(epic2SubTasks.contains(subTask4), "В эпике 2 отсутствует подзадача 2");
 
 
-        Epic TestUpdateEpic = new Epic("NewEpic 1", "Новое описание", 1, TaskStatus.NEW);
-        manager.updateEpic(TestUpdateEpic);
+        Epic testUpdateEpic = new Epic("NewEpic 1", "Новое описание", 1, TaskStatus.NEW);
+        manager.updateEpic(testUpdateEpic);
         Epic actualEpic = manager.getEpicById(1);
         assertEquals("NewEpic 1", actualEpic.getTaskName(), "Имя нового эпика ошибочно");
         assertEquals("Новое описание", actualEpic.getDescription(), "Описание нового эпика ошибочно");
         assertEquals(TaskStatus.NEW, actualEpic.getStatus(), "Статус нового эпика ошибочен");
 
-        SubTask TestUpdateSubTask1 = new SubTask("NewSubtask 1", "Новое описание", 2, TaskStatus.DONE, 1);
-        manager.updateSubTask(TestUpdateSubTask1);
+        SubTask testUpdateSubTask1 = new SubTask("NewSubtask 1", "Новое описание", 2, TaskStatus.DONE, 1);
+        manager.updateSubTask(testUpdateSubTask1);
         SubTask actualSubTask1 = manager.getSubTaskById(2);
         assertEquals("NewSubtask 1", actualSubTask1.getTaskName(), "Имя новой подзадачи ошибочен");
         assertEquals("Новое описание", actualSubTask1.getDescription(), "Описание новой подзадачи ошибочно");
         assertEquals(TaskStatus.DONE, actualSubTask1.getStatus(), "Статус новой подзадачи ошибочен");
         assertEquals(TaskStatus.IN_PROGRESS, actualEpic.getStatus(), "Статус эпика после апдейта подзадачи ошибочен");
 
-        SubTask TestUpdateSubTask2 = new SubTask("NewSubtask 2", "Новое описание", 3, TaskStatus.DONE, 1);
-        manager.updateSubTask(TestUpdateSubTask2);
+        SubTask testUpdateSubTask2 = new SubTask("NewSubtask 2", "Новое описание", 3, TaskStatus.DONE, 1);
+        manager.updateSubTask(testUpdateSubTask2);
         SubTask actualSubTask2 = manager.getSubTaskById(3);
         assertEquals("NewSubtask 2", actualSubTask2.getTaskName(), "Имя новой подзадачи ошибочен");
         assertEquals("Новое описание", actualSubTask2.getDescription(), "Описание новой подзадачи ошибочно");
@@ -163,7 +163,7 @@ class InMemoryTaskManagerTest {
         assertFalse(manager.containsSubTasks(6),"Подзадача 2 эпика не удалена");
 
         manager.deleteSubTaskById(3);
-        assertFalse(actualEpic.getSubTasks().contains(SubTask3),"Подзадача 3 из списка эпика не удалена");
+        assertFalse(actualEpic.getSubTasks().contains(subTask3),"Подзадача 3 из списка эпика не удалена");
         assertFalse(manager.containsSubTasks(3),"Подзадача 3 эпика не удалена");
     }
 
@@ -182,8 +182,8 @@ class InMemoryTaskManagerTest {
     @Test
     public void testEpicCleaning() {
         Epic epic1 = manager.createNewEpic(new Epic("Epic 1", "E", 1, TaskStatus.NEW));
-        SubTask SubTask1 = manager.createNewSubTask(new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1));
-        SubTask SubTask2 = manager.createNewSubTask(new SubTask("Subtask 2", "S2", 3, TaskStatus.NEW, 1));
+        SubTask subTask1 = manager.createNewSubTask(new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1));
+        SubTask subTask2 = manager.createNewSubTask(new SubTask("Subtask 2", "S2", 3, TaskStatus.NEW, 1));
 
         assertEquals(1, manager.getAllEpic().size(), "Эпик не добавлен");
         assertEquals(2, manager.getAllSubTasks().size(), "Подзадачи не добавлены");
@@ -198,8 +198,8 @@ class InMemoryTaskManagerTest {
     @Test
     public void testSubTasksCleaning() {
         Epic epic1 = manager.createNewEpic(new Epic("Epic 1", "E", 1, TaskStatus.NEW));
-        SubTask SubTask1 = manager.createNewSubTask(new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1));
-        SubTask SubTask2 = manager.createNewSubTask(new SubTask("Subtask 2", "S2", 3, TaskStatus.NEW, 1));
+        SubTask subTask1 = manager.createNewSubTask(new SubTask("Subtask 1", "S1", 2, TaskStatus.NEW, 1));
+        SubTask subTask2 = manager.createNewSubTask(new SubTask("Subtask 2", "S2", 3, TaskStatus.NEW, 1));
 
         assertEquals(1, manager.getAllEpic().size(), "Эпик не добавлен");
         assertEquals(2, manager.getAllSubTasks().size(),"Подзадачи не добавлены");
