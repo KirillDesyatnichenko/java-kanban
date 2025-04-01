141cfc5bddec93e4e1de0494df7a7620dbaed343
Kirill Desyatnichenko <desyatnichenkokirill@yandex.ru>
2025-04-01 17:43:09 +0400

    Версия менеджера задач Фин.задание 6 спринта. Исправлены опечатки в наименовании переменных в тестах. Исправлены опечатки в InMemoryTaskManager. Исправлены опечатки в InMemoryHistoryManager. Исправлен клас Node, тип Task изменен на T.

--- src/ru/yandex/practicum/TaskManager/Service/Node.java
+++ src/ru/yandex/practicum/TaskManager/Service/Node.java
@@ -1,11 +1,11 @@
 package ru.yandex.practicum.TaskManager.Service;
 
-public class Node<Task> {
-    public Task task;
-    public Node<Task> next;
-    public Node<Task> prev;
+public class Node<T> {
+    public T task;
+    public Node<T> next;
+    public Node<T> prev;
 
-    public Node(Node<Task> prev, Task task, Node<Task> next) {
+    public Node(Node<T> prev, T task, Node<T> next) {
         this.task = task;
         this.next = next;
         this.prev = prev;
